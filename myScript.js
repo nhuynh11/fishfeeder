69ded106e6185f3c8e09e89648f2904deb7a3167
nhuynh11 <nphuynh11@gmail.com>
2017-10-14 19:18:50 -0400

    added feed.js for arduino and index.html, myScript.js, styles.css for web user interface

---
+++ myScript.js
@@ -0,0 +1,6 @@
+<script src="//cdn.pubnub.com/pubnub-3.7.18.min.js"></script>
+// keys for pubnub
+var pubnub = PUBNUB({
+    　subscribe_key : 'you_subscribe_key',                          
+    　publish_key   : 'your_publish_key'
+});
